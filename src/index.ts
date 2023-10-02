f5ebaa514cb281fd5b3bbc923a8b99c477db68e1
christopherv123 <109395351+christopherv123@users.noreply.github.com>
2023-10-02 17:11:15 +0800

    change

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,50 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+app.post('/ticket', async (req, res) => {
+  try {
+    const {
+      tickettitle,
+      section,
+      roll,
+      seat,
+      imgname,
+      showdate,
+      venue,
+      tourtitle,
+      ticketheader,
+      entry
+    } = req.body;
+    const newTicket = await pool.query(
+      'INSERT INTO ticketsys (tickettitle,section,roll,seat,showdate,imgname,venue,tourtitle,ticketheader,entry) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING * ',
+     [tickettitle,section, roll, seat,showdate,imgname,venue,tourtitle,ticketheader,entry]
+    );
+    res.json(newTicket.rows[0]);
+  } catch (err) {
+    console.log('failed to add ticket');
+  }
+});
+
+//get all tickets
+app.get('/tickets', async(req, res) => {
+  try {
+    const allTickets = await pool.query('SELECT * FROM ticketsys');
+    res.json(allTickets.rows);
+  } catch (err) {
+    console.log('there is no ticket yet');
+  }
+});
+
+// //get a todo
+app.get('/ticket/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id =$1', [id]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 app.listen(port, () => {
